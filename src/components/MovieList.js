f9c3e36c71b37c56f0367e3e1ee51744baa67316
Charlie Sheppard <sheppard.charlie24@gmail.com>
2021-01-06 12:22:20 -0500

    add drop down selector

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 const MovieList = (props) => {
     const movies = props.movies.map(movie => {
-        return <div>{movie.title}</div>
+        const imagePath = `http://image.tmdb.org/t/p/w92/${movie.poster_path}`;
+
+        if (movie.poster_path === null) {
+            return <div key={movie.id}>{movie.title}</div>;
+        } else {
+            return (
+              <div key={movie.id}>
+                {movie.title}
+                <img src={imagePath} alt={movie.overview} />
+              </div>
+            );
+        }
     })
 
-    console.log(props);
     return <div>{movies}</div>
 }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    state = {term: ''};
+    state = {term: '', selectorOption: 'movie'};
 
     onFormSubmit = e => {
         e.preventDefault();
@@ -9,19 +9,60 @@ class SearchBar extends React.Component {
         this.props.onSubmit(this.state.term);
     }
 
+    onSelectorChange = e => {
+        this.setState({selectorOption: e.target.value});
+
+        // console.log(this.state.selectorOption);
+    }
+
     render() {
+        console.log(this.state.selectorOption);
         return (
-            <div className="search--form">
-                <form onSubmit={this.onFormSubmit} className="search--form__field">
-                    <label>Search Movies</label>
-                    <input 
-                        type="text"
-                        value={this.state.term}
-                        onChange={e => this.setState({term: e.target.value})}
-                    />
-                </form>
-            </div>
-        )
+          <div className="search--form">
+            <form onSubmit={this.onFormSubmit} className="search--form__field">
+              <label>Search Movies</label>
+              <select
+                className="content-selector-dropdown"
+                onChange={this.onSelectorChange}
+              >
+                <option
+                  className="content-selector-option"
+                  value="movie"
+                //   onClick={this.onSelectorChange}
+                >
+                  Movie
+                </option>
+                <option
+                  className="content-selector-option"
+                  value="tvshow"
+                //   onClick={this.onSelectorChange}
+                >
+                  TV Show
+                </option>
+                <option
+                  className="content-selector-option"
+                  value="actor/actress"
+                //   onClick={this.onSelectorChange}
+                >
+                  Actor/Actress
+                </option>
+                <option
+                  className="content-selector-option"
+                  value="director"
+                //   onClick={this.onSelectorChange}
+                >
+                  Director
+                </option>
+              </select>
+              <input
+                type="text"
+                value={this.state.term}
+                onChange={(e) => this.setState({ term: e.target.value })}
+              />
+              <button onClick={this.onFormSubmit}>Search</button>
+            </form>
+          </div>
+        );
     }
 }
 
